f005158a3e538e451cc8fca7d70a64f76c2a87f5
dmt0768 <63654413+dmt0768@users.noreply.github.com>
2020-08-22 16:21:54 +0300

    Change

--- synth_server/core/urls.py
+++ synth_server/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import show_main_page, create_line, delete_line, edit_line, stop_line
+from core.views import show_main_page, create_line, delete_line, edit_line, stop_line, AJAX_test, refresh_page
 
 urlpatterns = [
     path('', show_main_page, name='show_main_page'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('delete_page/', delete_line, name='delete_line'),
     path('edit_page/', edit_line, name='edit_line'),
     path('stop_page/', stop_line, name='stop_line'),
+    path('test/', AJAX_test),
+    path('refresh_page/', refresh_page, name='refresh_page')
 ]
--- synth_server/core/views.py
+++ synth_server/core/views.py
@@ -3,10 +3,27 @@ from django.shortcuts import render, redirect
 
 from core.models import Lines
 
+import random
+
+#_______________________________________
+def is_integer(n):
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
+
+#_______________________________________
+
+
 
 def show_main_page(request):
     content = Lines.objects.all()
     return render(request, 'core/Main_page.html', {'Lines':content})
+
+def refresh_page(request):
+    content = Lines.objects.all()
+    return render(request, 'core/Create_tmpl.html', {'Lines':content}) #redirect('show_main_page')
 
 def create_line(request):
 
@@ -15,19 +32,22 @@ def create_line(request):
         max_id = max([i.id for i in Lines.objects.all()]) + 1
     else: max_id = 0
     Lines.objects.create(id=max_id).save()
-    return redirect('show_main_page')
+    content = Lines.objects.all()
+
+    return render(request, 'core/Create_tmpl.html', {'Lines':content}) #redirect('show_main_page')
 
 def delete_line(request):
     victim = Lines.objects.filter(id=int(request.GET['delete']))[0]
     victim.delete()
-    return redirect('show_main_page')
+    content = Lines.objects.all()
+    return render(request, 'core/Create_tmpl.html', {'Lines':content})
 
 def edit_line(request):
     #print('\n\n\n\n\n' + str(request.GET) + '\n\n\n\n\n')
     edit = Lines.objects.filter(id=int(request.GET['edit']))[0]
     temp = request.GET
 
-    if (temp['number'] == 'None') or (temp['freq'] == 'None'):
+    if (is_integer(temp['freq']) != True) or (is_integer(temp['number']) != True): #if (temp['number'] == '') or (temp['freq'] == '') or (temp['freq'].is_integer() != True) or (temp['freq'].is_integer() != True):
         edit.status = 'Ошибка ввода'
         edit.save()
 
@@ -37,11 +57,16 @@ def edit_line(request):
         edit.status = 'Активно'
         edit.save()
 
-    #content = Lines.objects.all()
-    return redirect('show_main_page')
+    content = Lines.objects.all()
+    return render(request, 'core/Create_tmpl.html', {'Lines':content}) #redirect('show_main_page')
 
 def stop_line(request):
     stopped = Lines.objects.filter(id=int(request.GET['stop']))[0]
     stopped.status = 'Остановлено'
     stopped.save()
-    return redirect('show_main_page')
+    content = Lines.objects.all()
+    return render(request, 'core/Create_tmpl.html', {'Lines':content}) #redirect('show_main_page')
+
+def AJAX_test(request):
+
+    return HttpResponse(str(random.uniform(1,10)) + ' Гц')
